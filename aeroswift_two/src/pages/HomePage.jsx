0920afca003cb39348d84313bc0ec16edcf54f40
spicoding <shaneinduli@gmail.com>
2025-07-22 10:19:28 +0300

    Pre-release

--- aeroswift_two/src/pages/HomePage.jsx
+++ aeroswift_two/src/pages/HomePage.jsx
@@ -7,8 +7,7 @@ import FlightCard from '../components/FlightCard';
 
 const HomePage = ({ flights = [] }) => {  // Added default value
   const featuredFlights = flights.slice(0, 3);
-  
-  // ... rest of your component
+ 
   return (
     <div className="container mx-auto px-4 py-8">
       {/* Hero Section */}
